af5a4b29421879f43dd6d60dc6dcd59122ccc967
Nicholas Sullivan <videogamer_9@msn.com>
2019-10-02 18:52:13 -0400

    Add files via upload

---
+++ Brain.java
@@ -0,0 +1,17 @@
+
+public class Brain {
+
+	public static void main(String[] args) {
+		int a;	int b;
+		for (a=1; a<=12; ++a)
+		{
+		    for (b=1; b<=12; ++b)
+		    {
+		        System.out.print(a*b+"  ");  
+		    }
+		    System.out.println();
+		}    
+
+	}
+
+}
